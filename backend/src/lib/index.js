242197929cf08512428106830e5564531e217b7e
next-diegotavera <diegoalan.tavera.next@bbva.com>
2020-09-07 20:27:49 -0500

    deleted unused files

--- backend/src/lib/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.CustomError = (message, code = 500) => {
-    const err = new Error(message);
-    err.code = code;
-    return err;
-};
--- backend/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-
-require('../api/posts')(router);
-require('../api/auth')(router);
-
-module.exports = router;
